fb71f670c9ee0519b420b5029ecb8b3a4fb9715e
Chuanche <s824132394@gmail.com>
2023-06-17 15:44:12 -0700

    A2-1

--- Assignment/A2/A2-DriverExam/Form1.Designer.cs
+++ Assignment/A2/A2-DriverExam/Form1.Designer.cs
@@ -28,12 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            openFileDialog1 = new OpenFileDialog();
+            importFileButton = new Button();
+            label1 = new Label();
+            ScoreBox = new TextBox();
+            DetailBox = new ListBox();
+            SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // importFileButton
+            // 
+            importFileButton.Location = new Point(192, 12);
+            importFileButton.Name = "importFileButton";
+            importFileButton.Size = new Size(112, 34);
+            importFileButton.TabIndex = 0;
+            importFileButton.Text = "Import File";
+            importFileButton.UseVisualStyleBackColor = true;
+            importFileButton.Click += importFileButton_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(234, 60);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 24);
+            label1.TabIndex = 1;
+            // 
+            // ScoreBox
+            // 
+            ScoreBox.Location = new Point(175, 60);
+            ScoreBox.Name = "ScoreBox";
+            ScoreBox.ReadOnly = true;
+            ScoreBox.Size = new Size(150, 30);
+            ScoreBox.TabIndex = 2;
+            ScoreBox.TextAlign = HorizontalAlignment.Center;
+            // 
+            // DetailBox
+            // 
+            DetailBox.FormattingEnabled = true;
+            DetailBox.ItemHeight = 24;
+            DetailBox.Location = new Point(46, 118);
+            DetailBox.Name = "DetailBox";
+            DetailBox.Size = new Size(387, 292);
+            DetailBox.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(11F, 24F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(489, 450);
+            Controls.Add(DetailBox);
+            Controls.Add(ScoreBox);
+            Controls.Add(label1);
+            Controls.Add(importFileButton);
+            Name = "Form1";
+            Text = "Dirver Exam";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private OpenFileDialog openFileDialog1;
+        private Button importFileButton;
+        private Label label1;
+        private TextBox ScoreBox;
+        private ListBox DetailBox;
     }
 }
--- Assignment/A2/A2-DriverExam/Form1.cs
+++ Assignment/A2/A2-DriverExam/Form1.cs
@@ -6,5 +6,54 @@ namespace A2_DriverExam
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void importFileButton_Click(object sender, EventArgs e)
+        {
+            string[] answerArr = { "B", "D", "A", "A", "C", "A", "B", "A", "C", "D", "B", "C", "D", "A", "D", "C", "C", "B", "D", "A" };
+            OpenFileDialog openFile = new OpenFileDialog();
+            StreamReader inputFile = null;
+            int count = 0;
+            const int total = 20;
+            int i = 0;
+            string[] userAnswer = new string[20];
+            DetailBox.Items.Add("#\tAttempt\tAnswer");
+
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                inputFile = File.OpenText(openFile.FileName);
+            }
+
+            while (inputFile.ReadLine != null && i <= 19)
+            {
+                userAnswer[i] = inputFile.ReadLine();
+                i++;
+            }
+
+            for (int j = 0; j < answerArr.Length; j++)
+            {
+                if (answerArr[j] == userAnswer[j])
+                {
+                    count++;
+                }
+                else
+                {
+                    DetailBox.Items.Add($"{j + 1}\t{userAnswer[j]}\t{answerArr[j]}");
+                }
+            }
+            ScoreBox.Text = ($"{count}/{total}");
+            if (count >= 15)
+            {
+                MessageBox.Show("You passed the exam, well done!");
+            }
+            else
+            {
+                MessageBox.Show("You failed, good luck next time.");
+            }
+        }
     }
 }
